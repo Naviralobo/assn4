1c9aabf5dbcce0ab622787ff3f46b60f8faa7816
Navira Lobo <majel.josephine@gmail.com>
2022-08-03 12:34:50 +0530

    modified dom1

--- dom.js
+++ dom.js
@@ -12,10 +12,28 @@
 //console.log(document.forms);
 //console.log(document.links);
 //console.log(document.images);
-var header = document.getElementById('main-header');
-header.style.borderBottom='solid 3px #000';
+//var header = document.getElementById('main-header');
+//header.style.borderBottom='solid 3px #000';
 
-var titles = document.getElementsByClassName('title');
-titles[1].style.fontWeight='bold';
-titles[1].style.color='green';
+//var titles = document.getElementsByClassName('title');
+//titles[1].style.fontWeight='bold';
+//titles[1].style.color='green';
+
+
+var items = document.getElementsByClassName('list-group-item');
+undefined
+console.log(items);
+
+console.log(items[1]);
+
+items[1].textContent='Hello 2';
+
+items[0].style.fontWeight='bold';
+
+items[1].style.backgroundColor='yellow';
+
+for(var i=0; i<items.length ;i++){
+    items[i].style.backgroundColor='#f4f4f4';
+}
+
 
