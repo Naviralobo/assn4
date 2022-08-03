0beb826626718fa9a2dbdb81cbf908b3e2844d11
Navira Lobo <majel.josephine@gmail.com>
2022-08-03 12:43:59 +0530

    ans-assn4

--- dom.js
+++ dom.js
@@ -36,4 +36,8 @@ for(var i=0; i<items.length ;i++){
     items[i].style.backgroundColor='#f4f4f4';
 }
 
+items[2].style.backgroundColor='green';
+for(var i=0; i<items.length ;i++){
+    items[i].style.fontWeight='bold';
+}
 
