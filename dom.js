f35870a86e6dc2dcb2e567355db74b648c8dcf3e
Navira Lobo <majel.josephine@gmail.com>
2022-08-03 10:34:49 +0530

    assn4

---
+++ dom.js
@@ -0,0 +1,21 @@
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
+//document.title=123;
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.all[10]);
+//document.all[10].textcontent = 'Hello';
+//console.log(document.forms);
+//console.log(document.links);
+//console.log(document.images);
+var header = document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+
+var titles = document.getElementsByClassName('title');
+titles[1].style.fontWeight='bold';
+titles[1].style.color='green';
+
