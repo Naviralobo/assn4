e6cd9fc78586d514671817cdc30423af0d482ad8
Navira Lobo <majel.josephine@gmail.com>
2022-08-05 14:21:49 +0530

    ans-assn11

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
 
+
+//myForm.addEventListener('submit', onSubmit);
+//function onSubmit(e) {
+ // e.preventDefault();
+ // localStorage.setItem('UserName',nameInput.value);
+  //localStorage.setItem('UserEmail',emailInput.value);
+//}
+
+
+
+const myForm = document.querySelector('#my-form');
+//console.log("myObject",myObjSerialized);
 myForm.addEventListener('submit', onSubmit);
 function onSubmit(e) {
-  e.preventDefault();
-  localStorage.setItem('UserName',nameInput.value);
-  localStorage.setItem('UserEmail',emailInput.value);
+ e.preventDefault(); 
+ 
+const nameInput = document.querySelector('#name').value;
+const emailInput = document.querySelector('#email').value;
+const myObj={
+  nameInput,
+ emailInput
+};
+let myObjSerialized = JSON.stringify(myObj);
+  //console.log("myObject",myObj);
+  localStorage.setItem("myObject",myObjSerialized);
 }
+
 
